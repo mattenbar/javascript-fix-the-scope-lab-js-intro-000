fdda557fd8bf750a2e647173ed4e2ad2d07d92d2
Matthew Enbar <matthew.enbar@gmail.com>
2020-03-18 14:40:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,8 +19,6 @@ function add2(n) {
   const two = 2
   return n + two
 
-  // Feel free to move things around!
-
 }
 
 var funkyFunction = function() {
@@ -31,4 +29,6 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-theFunk = funkyFunction ()
+theFunk = funkyFunction (){
+  return 'FUNKY!'
+}
